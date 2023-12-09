5d5028a894513ccd7011cbd28bfc663ea152e494
vltir <87820976+vltir@users.noreply.github.com>
2023-12-09 18:46:20 +0100

    Day 9 finished

--- src/main/java/com/github/vltir/advent_of_code/day9/Day9.java
+++ src/main/java/com/github/vltir/advent_of_code/day9/Day9.java
@@ -9,29 +9,31 @@ import java.util.List;
 
 public class Day9 implements Day {
     private List<List<Integer>> lines = new ArrayList<>();
-    @Override
-    public String run(boolean stage2) {
+
+    public Day9() {
         List<String> inputList = InputReader.readInput("./src/main/java/com/github/vltir/advent_of_code/day9/input.txt");
         parseInput(inputList);
-        int sum =0;
+    }
+
+    @Override
+    public String run(boolean stage2) {
+        int sum = 0;
         for (List<Integer> line : lines) {
-            sum += followingOf(line);
+            sum += followingOf(line, stage2);
         }
-
-        return sum +"";
+        return sum + "";
     }
 
-    private int followingOf(List<Integer>line){
-        if(line.stream().allMatch(i->i==0))return 0;
+    private int followingOf(List<Integer> line, boolean front) {
+        if (line.stream().allMatch(i -> i == 0)) return 0;
         List<Integer> steps = new ArrayList<>();
-        for (int i = 0; i < line.size()-1; i++) {
-            steps.add(line.get(i+1)-line.get(i));
+        for (int i = 0; i < line.size() - 1; i++) {
+            steps.add(line.get(i + 1) - line.get(i));
         }
-        return line.get(line.size()-1)+followingOf(steps);
+        return line.get(front ? 0 : line.size() - 1) + (front ? -1 : 1) * followingOf(steps, front);
     }
 
-    private void parseInput(List<String> inputList){
-        inputList.stream().forEach(l-> lines.add(Arrays.stream(l.split(" +")).map(s->Integer.parseInt(s)).toList()));
+    private void parseInput(List<String> inputList) {
+        inputList.stream().forEach(l -> lines.add(Arrays.stream(l.split(" +")).map(s -> Integer.parseInt(s)).toList()));
     }
-
 }
